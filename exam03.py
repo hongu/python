4634fa43835ea9c63b461011d5ef2e97f379cf82
hongu83 <sudo83@gmail.com>
2020-10-24 16:30:02 +0900

    exam03 commit

--- exam03.py
+++ exam03.py
@@ -1,12 +1,19 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 
-class Exam(QWidget):
+class Exam(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
     def initUI(self):
-        self.resize(450,450)
+        self.statusBar()
+        self.statusBar().showMessage("안녕하세요")
+
+        menu = self.menuBar()
+        menu_file = menu.addMenu('File')
+        menu_file = menu.addMenu('Edit')
+
+        self.resize(450,400)
         self.show()
 
 app = QApplication(sys.argv)
