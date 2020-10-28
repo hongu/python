ffdf22a118236bf3603ac4f1cd8731d47ee74af4
hongu83 <sudo83@gmail.com>
2020-10-28 09:25:51 +0900

    exam03 add code

--- exam03.py
+++ exam03.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QMenu
+from PyQt5.QtCore import QCoreApplication
 
 class Exam(QMainWindow):
     def __init__(self):
@@ -9,9 +10,25 @@ class Exam(QMainWindow):
         self.statusBar()
         self.statusBar().showMessage("안녕하세요")
 
-        menu = self.menuBar()
-        menu_file = menu.addMenu('File')
-        menu_file = menu.addMenu('Edit')
+        menu = self.menuBar()                   #메뉴바 생성 
+        menu_file = menu.addMenu('File')        #그룹 생성
+        menu_Edit = menu.addMenu('Edit')        #그룹 생성
+
+        file_exit = QAction('Exit', self)       #메뉴 객체 생성
+        file_exit.setShortcut('Ctrl+Q')
+        file_exit.setStatusTip("누르면 잘가~")
+        new_txt = QAction('텍스트 파일', self)
+        new_py = QAction('파이썬 파일', self)
+
+        file_exit.triggered.connect(QCoreApplication.instance().quit)
+
+        file_new = QMenu('New', self)           #서브 그룹 추가
+
+        file_new.addAction(new_txt)             #서브 메뉴 추가
+        file_new.addAction(new_py)
+        
+        menu_file.addMenu(file_new)             #메뉴 등록 NEW
+        menu_file.addAction(file_exit)          #메뉴 등록 EXIT
 
         self.resize(450,400)
         self.show()
